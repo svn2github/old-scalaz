32a986ed99c304f2f92fb62685c9d420135fa5fe
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-03-24 08:11:34 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@171 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ artifacts/3.4/src/scalaz/Version.scala
@@ -0,0 +1,22 @@
+// Copyright Tony Morris 2008-2009
+// This software is released under an open source BSD licence.
+
+// $LastChangedRevision$
+// $LastChangedDate$
+
+
+package scalaz
+
+/**
+ * Prints the version of this software.
+ *
+ * @author <a href="mailto:code@tmorris.net">Tony Morris</a>
+ * @version $LastChangedRevision<br>
+ *          $LastChangedDate<br>
+ *          $LastChangedBy$
+ */
+object Version extends Application {
+  println("Scalaz version ${build.number}")
+  println("Compiled against Scala version 2.7.3")
+  println("Copyright Tony Morris 2008-2009")
+}
