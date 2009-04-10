9658d6fbe524b24db12d342b1db9fb0e7395e86a
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-10 09:31:16 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@181 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/HK.scala
+++ branches/HK.scala
@@ -7,7 +7,7 @@ object Identity {
     val a = x
   }
 
-  implicit def Di[A](i: Identity[A]) = i.a
+  implicit def di[A](i: Identity[A]) = i.a
 }
 
 trait Functor[F[_]] {
@@ -21,11 +21,53 @@ object Functor {
 }
 
 trait Pure[P[_]] {
-  def pure[A](a: A): P[A]
+  implicit def pure[A](a: A): P[A]
 }
 
 object Pure {
   implicit val IdentityPure = new Pure[Identity] {
     def pure[A](a: A) = Identity.id(a)
+  }
+}
+
+sealed trait Pointed[P[_]] {
+  val functor: Functor[P]
+  val pure: Pure[P]
+}
+
+object Pointed {
+  def pointed[P[_]](implicit f: Functor[P], p: Pure[P]) = new Pointed[P] {
+    val functor = f
+    val pure = p
+  }
+
+  implicit val IdentityPointed = pointed[Identity]
+}
+
+trait Apply[Z[_]] {
+  def apply[A, B](f: Z[A => B], a: Z[A]): Z[B]
+}
+
+object Apply {
+  implicit val IdentityApply: Apply[Identity] = new Apply[Identity] {
+    def apply[A, B](f: Identity[A => B], a: Identity[A]): Identity[B] = Identity.id(f(a))
+  }
+}
+
+sealed trait Applicative[Z[_]] {
+  implicit val pure: Pure[Z]
+  implicit val apply: Apply[Z]
+
+  implicit val functor: Functor[Z] = new Functor[Z] {
+    def fmap[A, B](fa: Z[A], f: A => B) = apply(pure.pure(f), fa)
+  }
+
+  val pointed = Pointed.pointed[Z]
+}
+
+object Applicative {
+  def applicative[Z[_]](implicit p: Pure[Z], a: Apply[Z]) = new Applicative[Z] {
+    val pure = p
+    val apply = a
   }
 }
