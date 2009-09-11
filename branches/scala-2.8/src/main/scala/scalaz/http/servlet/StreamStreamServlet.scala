88689ae408e3da6493194df2ed012cb4337567e4
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-09-11 05:34:35 +0000

    re-drop scalaz.http._
    It doesn't compile at the moment, due to https://lampsvn.epfl.ch/trac/scala/ticket/2308
    
    git-svn-id: http://scalaz.googlecode.com/svn@833 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scala-2.8/src/main/scala/scalaz/http/servlet/StreamStreamServlet.scala
+++
@@ -1,9 +0,0 @@
-package scalaz
-package http
-package servlet
-
-import HttpServletRequest._
-import HttpServletResponse._
-
-final class StreamStreamServlet extends
-  ScalazServlet[Stream, Stream, StreamStreamServletApplication](classOf[StreamStreamServletApplication])
