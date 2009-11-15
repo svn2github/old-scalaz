a800602d8465bc528d8be16654928f73cc5410d2
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-15 21:21:49 +0000

    Use scala version from scala-tools rather than from local system. Remove http and scapps modules, they need higher kinded existentials.
    git-svn-id: http://scalaz.googlecode.com/svn@878 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scala-2.8-modular/project/build/ScalazProject.scala
+++ branches/scala-2.8-modular/project/build/ScalazProject.scala
@@ -4,12 +4,16 @@ import java.util.jar.Attributes.Name._
 import java.io.File
 
 abstract class ScalazDefaults(info: ProjectInfo, component: String) extends DefaultProject(info) {
+  val scalaTools2_8_0Snapshots = Resolver.url("2.8.0 snapshots") artifacts "http://scala-tools.org/repo-snapshots/org/scala-lang/[module]/2.8.0-SNAPSHOT/[artifact]-[revision].[ext]"
+
+  // This lets you use a local copy of scala. Set build.scala.versions=2.8.0-latest in build.properties.
+  // override def localScala = defineScala("2.8.0-latest", Path.userHome / "usr" / "scala-2.8.0.latest" asFile) :: Nil
+
   override def compileOptions = target(Target.Java1_5) :: Unchecked :: super.compileOptions.toList
 
   override def packageOptions = ManifestAttributes((IMPLEMENTATION_TITLE, "Scalaz"), (IMPLEMENTATION_URL, "http://code.google.com/p/scalaz"), (IMPLEMENTATION_VENDOR, "The Scalaz Project"), (SEALED, "true")) :: Nil
 
   override def documentOptions = documentTitle("Scalaz " + component + projectVersion + " API Specification") :: windowTitle("Scalaz " + projectVersion) :: super.documentOptions.toList
-  //  override def defaultJarBaseName = "scalaz-" + component.toLowerCase + "-" + version.toString
 
   override def managedStyle = ManagedStyle.Maven
 
@@ -18,35 +22,27 @@ abstract class ScalazDefaults(info: ProjectInfo, component: String) extends Defa
   override def packageSrcJar = defaultJarPath("-sources.jar")
 
   val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
+
   val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadoc"), Nil, None)
-  val scalaTools2_8_0Snapshots = Resolver.url("2.8.0 snapshots") artifacts "http://scala-tools.org/repo-snapshots/org/scala-lang/[module]/2.8.0-SNAPSHOT/[artifact]-[revision].[ext]"
 
   override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
-
-  override def localScala = defineScala("2.8.0-latest", new File("/Users/jason/usr/scala-2.8.0.latest")) :: Nil
-
-//  override def fork = Some(new ForkScalaCompiler {
-//    override def javaHome: Option[File] = None
-//
-//    override def scalaJars: Iterable[File] = List(
-//      new File("/Users/jason/usr/scala-2.8.0.latest/lib/scala-compiler.jar"),
-//      new File("/Users/jason/usr/scala-2.8.0.latest/lib/scala-library.jar")
-//      )
-//  })
 }
 
 final class ScalazProject(info: ProjectInfo) extends ParentProject(info) {
   // Sub-projects
   lazy val core = project("core", "Scalaz Core", new ScalazCoreProject(_))
   lazy val test = project("test", "Scalaz Test", new ScalazTestProject(_), core)
-  lazy val http = project("http", "Scalaz HTTP", new ScalazHttpProject(_), core)
-  lazy val scapps = project("scapps", "Scalaz Scapps", new ScalazScappsProject(_), core, http)
 
-  // Use this as a test run of pom generation and jar publishing
-  // val publishTo = Resolver.file("local-file-repo", new java.io.File("/Users/jason/code/scalaz-maven/snapshots"))
+  // Blocked by http://lampsvn.epfl.ch/trac/scala/ticket/2308
+  //  lazy val http = project("http", "Scalaz HTTP", new ScalazHttpProject(_), core)
+  //  lazy val scapps = project("scapps", "Scalaz Scapps", new ScalazScappsProject(_), core, http)
+
 
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+
+  // Use this instead as a test run of pom generation and jar publishing
+  // val publishTo = Resolver.file("local-file-repo", new java.io.File("/Users/jason/code/scalaz-maven/snapshots"))
 
   override def publishAction = task {None}
 
@@ -63,14 +59,11 @@ final class ScalazProject(info: ProjectInfo) extends ParentProject(info) {
 protected final class ScalazCoreProject(info: ProjectInfo) extends ScalazDefaults(info, "Core")
 
 protected final class ScalazTestProject(info: ProjectInfo) extends ScalazDefaults(info, "Test") {
-  val fjRepo = "Functional Java Repository" at "http://functionaljava.googlecode.com/svn/maven"
-  val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.0-20091013.004057-+" % "1.7-SNAPSHOT" withSources
-  val functionaljava = "org.functionaljava" % "fj" % "2.19" withSources
+  val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.0-20091106.025327-+" % "1.7-SNAPSHOT" withSources
 }
 
 protected final class ScalazHttpProject(info: ProjectInfo) extends ScalazDefaults(info, "HTTP") {
   val servlet = "javax.servlet" % "servlet-api" % "2.5" withSources
 }
 
-protected final class ScalazScappsProject(info: ProjectInfo) extends ScalazDefaults(info, "Scapps") {
-}
+protected final class ScalazScappsProject(info: ProjectInfo) extends ScalazDefaults(info, "Scapps")
