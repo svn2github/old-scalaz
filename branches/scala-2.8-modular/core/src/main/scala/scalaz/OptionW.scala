e0ac9e8c48806c74b5fd4150981ce6372166d284
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-24 23:56:34 +0000

    Delegate OptionW.toNull to Option.orNull. Add an type constraint so this can only be called on nullable types (i.e. not value types or reference types that derive from NotNull.)
    git-svn-id: http://scalaz.googlecode.com/svn@891 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scala-2.8-modular/core/src/main/scala/scalaz/OptionW.scala
+++ branches/scala-2.8-modular/core/src/main/scala/scalaz/OptionW.scala
@@ -31,7 +31,7 @@ sealed trait OptionW[A] {
 
   def |(a: => A) = value getOrElse a
 
-  def toNull = value getOrElse null.asInstanceOf[A]
+  def toNull[A1 >: A](implicit ev: Null <:< A1): A1 = value orNull
 
   def unary_~(implicit z: Zero[A]) = value getOrElse z.zero
   
