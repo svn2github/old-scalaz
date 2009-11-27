def1d4cd78190f47de7a6bc9385001a95d0f74d1
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-27 03:44:25 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@901 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/Scalaz5.scala
+++ branches/scalaz5/Scalaz5.scala
@@ -179,23 +179,16 @@ object Example {
     println(List(40, 50, 60) ⊛ (List(1, 2, 3) ∘ ((_: Int) * (_: Int)).curry))
 
     // Applicative functor lift
-    {
-      println(List(1, 2, 3) <⊛> (List(40, 50, 60), (_: Int) * (_: Int)))
-    }
+
+    println(List(1, 2, 3) <⊛> (List(40, 50, 60), (_: Int) * (_: Int)))
 
     // Applicative functor lift to pair
-    {
-      println(List(1, 2, 3) <<⊛>> List(40, 50, 60))
-    }
+    println(List(1, 2, 3) <<⊛>> List(40, 50, 60))
 
     // Applicative functor lift (anonymous right)
-    {
-      println(List(1, 2, 3) ⊛> List(40, 50, 60))
-    }
+    println(List(1, 2, 3) ⊛> List(40, 50, 60))
 
     // Applicative functor lift (anonymous left)
-    {
-      println(List(1, 2, 3) <⊛ List(40, 50, 60))
-    }
+    println(List(1, 2, 3) <⊛ List(40, 50, 60))
   }
 }
