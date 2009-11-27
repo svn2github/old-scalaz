b0400cc0cbb67753d26ef9d2aca5e09d749983a7
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-27 22:23:07 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@917 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/Build.hs
+++ branches/scalaz5/Build.hs
@@ -5,6 +5,9 @@ import Lastik.Runner
 import Lastik.Output
 import Lastik.Util
 import System.FilePath
+import System.Cmd
+import System.Exit
+import Data.List
 
 exampleDir = "src" </> "example"
 mainDir = "src" </> "main"
@@ -15,20 +18,42 @@ buildExample = "build" </> "example"
 buildMain = "build" </> "main"
 buildTest = "build" </> "test"
 
+cp :: String
+cp = "classpath" ~?? [buildExample, buildMain, buildTest]
+
+main' :: Scalac
 main' = scalac {
   directory = Just buildMain
 }
 
+main :: IO ExitCode
 main = main' +->- [mainDir]
 
+example' :: Scalac
 example' = main' >=>=> scalac {
   directory = Just buildExample
 }
 
+example :: IO ExitCode
 example = main >>>> (example' +->- [exampleDir])
 
+test' :: Scalac
 test' = main' >=>=> scalac {
   directory = Just buildTest
 }
 
+test :: IO ExitCode
 test = main >>>> (test' +->- [testDir])
+
+-- todo scala function in Lastik
+scala :: String -> IO ExitCode
+scala k = system ("scala " ++ k)
+
+runExample :: String -> IO ExitCode
+runExample e = example >>>> scala (intercalate " " [cp, e])
+
+allExample :: IO ExitCode
+allExample = runExample "scalaz.Example"
+
+repl :: IO ExitCode
+repl = scala cp
