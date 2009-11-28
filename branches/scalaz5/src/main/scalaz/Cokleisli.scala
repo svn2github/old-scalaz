ecb5977b8377f6e3feff74f8eb9086d16e9826e0
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-28 04:45:14 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@948 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ branches/scalaz5/src/main/scalaz/Cokleisli.scala
@@ -0,0 +1,15 @@
+package scalaz
+
+trait Cokleisli[W[_], A, B] {
+  def apply(a: W[A]): B
+
+  import Scalaz._
+
+  def <<=(a: W[A])(implicit w: Comonad[W]) = a ⇤ apply
+}
+
+object Cokleisli {
+  def cokleisli[W[_], A, B](f: W[A] => B) = new Cokleisli[W, A, B] {
+    def apply(a: W[A]) = f(a)
+  }
+}
