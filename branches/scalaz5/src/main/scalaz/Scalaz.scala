87b09444cac6571e47add7ecd59c2abb435d63c3
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-28 22:31:54 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@977 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/src/main/scalaz/Scalaz.scala
+++ branches/scalaz5/src/main/scalaz/Scalaz.scala
@@ -19,6 +19,7 @@ object Scalaz extends ScalazLow
               with    MetricSpaces
               with    Zeros
               with    CharSets
+              with    ArrayBytes
               with    Chars {
   def FunctorBindApply[Z[_]](implicit t: Functor[Z], b: Bind[Z]) = new Apply[Z] {
     def apply[A, B](f: Z[A => B], a: Z[A]): Z[B] = {
