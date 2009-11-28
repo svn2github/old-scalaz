892d8ca50db5479dc72e27e2a5235ecb4ab2d440
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-28 05:37:17 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@961 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/src/main/scalaz/Scalaz.scala
+++ branches/scalaz5/src/main/scalaz/Scalaz.scala
@@ -14,6 +14,7 @@ object Scalaz extends ScalazLow
               with    Function2s
               with    InputStreams
               with    Longs
+              with    States
               with    Chars {
   def FunctorBindApply[Z[_]](implicit t: Functor[Z], b: Bind[Z]) = new Apply[Z] {
     def apply[A, B](f: Z[A => B], a: Z[A]): Z[B] = {
