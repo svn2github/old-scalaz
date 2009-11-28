731ad3c8c656a58ecd4e2343ce1813bdad1975d1
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-28 04:21:11 +0000

    essential parts of this bug..
    git-svn-id: http://scalaz.googlecode.com/svn@944 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/hk-infer-bug/scalaz5/src/example/scalaz/HigherKindInferenceBug.scala
+++ branches/hk-infer-bug/scalaz5/src/example/scalaz/HigherKindInferenceBug.scala
@@ -2,16 +2,18 @@ package scalaz
 
 
 object HigherKindInferenceBug {
-  def main(args: Array[String]) = run
-
   import Scalaz._
+  
+  List(1, 2, 3).v // MA.ma inferred
 
-  def run {
-    {
-      implicit val a = List[Int]()
-      ma(List(1, 2, 3)) foo
-//      List(1, 2, 3).foo // error: scalaz is not an enclosing class
-//      List(1, 2, 3) foo // error: foo is not a member of Int
-    }
-  }
+  implicit val a = List[Int]()
+
+  // again relying on implicit conversion to MA. This time we call try to call foo
+  // List(1, 2, 3).foo // error: scalaz is not an enclosing class
+  // List(1, 2, 3) foo // error: foo is not a member of Int
+
+  // These compile correctly.
+  ma(List(1, 2, 3)).foo
+  ma(List(1, 2, 3)) foo
+
 }
