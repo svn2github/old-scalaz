115a4030fcf827e4118da23bfa599352ed24e9a0
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-29 08:10:28 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@1022 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/src/main/scalaz/Length.scala
+++ branches/scalaz5/src/main/scalaz/Length.scala
@@ -5,8 +5,81 @@ trait Length[-L[_]] {
 }
 
 object Length {
+  import Scalaz._
+
+  implicit val IdentityLength: Length[Identity] = new Length[Identity] {
+    def len[A](a: Identity[A]) = 1
+  }
+
+  implicit def NonEmptyListLength[A]: Length[NonEmptyList] = new Length[NonEmptyList] {
+    def len[A](a: NonEmptyList[A]) = a.list.length
+  }
+
+  implicit val ZipStreamLength: Length[ZipStream] = new Length[ZipStream] {
+    def len[A](a: ZipStream[A]) = a.value.length
+  }
+
+  implicit val Tuple1Length: Length[Tuple1] = new Length[Tuple1] {
+    def len[A](a: Tuple1[A]) = 1
+  }
+
+  implicit val Function0Length: Length[Function0] = new Length[Function0] {
+    def len[A](a: Function0[A]) = 1
+  }
+
+  implicit val OptionLength: Length[Option] = new Length[Option] {
+    def len[A](a: Option[A]) = a map (_ => 1) getOrElse 0
+  }
+
+  implicit def EitherLeftLength[X]: Length[PartialApply1Of2[Either.LeftProjection, X]#Flip] = new Length[PartialApply1Of2[Either.LeftProjection, X]#Flip] {
+    def len[A](a: Either.LeftProjection[A, X]) = a.e match {
+      case Right(_) => 0
+      case Left(_) => 1
+    }
+  }
+
+  implicit def EitherRightLength[X]: Length[PartialApply1Of2[Either.RightProjection, X]#Apply] = new Length[PartialApply1Of2[Either.RightProjection, X]#Apply] {
+    def len[A](a: Either.RightProjection[X, A]) = a.e match {
+      case Right(_) => 1
+      case Left(_) => 0
+    }
+  }
+
+  implicit def ValidationLength[X]: Length[PartialApply1Of2[Validation, X]#Apply] = new Length[PartialApply1Of2[Validation, X]#Apply] {
+    def len[A](a: Validation[X, A]) = a match {
+      case Success(_) => 1
+      case Failure(_) => 0
+    }
+  }
+
+  implicit def ValidationFailureLength[X]: Length[PartialApply1Of2[FailProjection, X]#Flip] = new Length[PartialApply1Of2[FailProjection, X]#Flip] {
+    def len[A](a: FailProjection[A, X]) = a.validation match {
+      case Success(_) => 0
+      case Failure(_) => 1
+    }
+  }
+
+  /* todo
+  implicit val ArrayLength = new Length[Array] {
+    def len[A](a: Array[A]) = a.length
+  }
+  */
+
   implicit val IterableLength: Length[Iterable] = new Length[Iterable] {
     def len[A](a: Iterable[A]) = {
+      var n = 0
+      val i = a.iterator
+      while(i.hasNext) {
+        n = n + 1
+        i.next
+      }
+
+      n
+    }
+  }
+
+  implicit val JavaIterableLength: Length[java.lang.Iterable] = new Length[java.lang.Iterable] {
+    def len[A](a: java.lang.Iterable[A]) = {
       var n = 0
       val i = a.iterator
       while(i.hasNext) {
