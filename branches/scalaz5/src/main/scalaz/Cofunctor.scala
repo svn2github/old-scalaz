af7e81f2a49fdbf4b36b1e66d8b377ce38d3275a
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-29 02:47:16 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@1003 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/src/main/scalaz/Cofunctor.scala
+++ branches/scalaz5/src/main/scalaz/Cofunctor.scala
@@ -11,7 +11,29 @@ object Cofunctor {
     def comap[A, B](r: A => X, f: B => A) = r compose f
   }
 
+  implicit val EqualCofunctor: Cofunctor[Equal] = new Cofunctor[Equal] {
+    def comap[A, B](r: Equal[A], f: B => A) = equal[B]((b1, b2) => r equal (f(b1), f(b2)))
+  }
+
+  implicit val OrderCofunctor: Cofunctor[Order] = new Cofunctor[Order] {
+    def comap[A, B](r: Order[A], f: B => A) = order[B]((b1, b2) => r order (f(b1), f(b2)))
+  }
+
+  implicit val ShowCofunctor: Cofunctor[Show] = new Cofunctor[Show] {
+    def comap[A, B](r: Show[A], f: B => A) = show[B](b => r show (f(b)))
+  }
+
   implicit val MetricSpaceCofunctor: Cofunctor[MetricSpace] = new Cofunctor[MetricSpace] {
     def comap[A, B](r: MetricSpace[A], f: B => A) = metricSpace[B]((b1, b2) => r distance (f(b1), f(b2)))
   }
+
+  /* todo
+  implicit val ActorCofunctor = new Cofunctor[Actor] {
+    def comap[A, B](r: Actor[A], f: B => A): Actor[B] = Actor.actor[B](r.onError, (b: B) => (r ! f(b))())(r.strategy)
+  }
+
+  implicit val EffectCofunctor = new Cofunctor[Effect] {
+    def comap[A, B](r: Effect[A], f: B => A) = Effect.effect[B]((b) => r ! f(b))(r.strategy)
+  }
+  */
 }
--- branches/scalaz5/src/main/scalaz/Semigroup.scala
+++ branches/scalaz5/src/main/scalaz/Semigroup.scala
@@ -14,140 +14,140 @@ object Semigroup {
   import Scalaz._
   import xml.{Elem, Node, NodeSeq}
 
-  implicit val DigitSemigroup: Semigroup[Digit] = semigroup[Digit]((a, b) => a.toInt + b.toInt)
+  implicit val DigitSemigroup: Semigroup[Digit] = semigroup((a, b) => a.toInt + b.toInt)
 
-  implicit val OrderingSemigroup: Semigroup[Ordering] = semigroup[Ordering] {
+  implicit val OrderingSemigroup: Semigroup[Ordering] = semigroup {
     case (EQ, a) => a
     case (LT, _) => LT
     case (GT, _) => GT
   }
 
-  implicit val UnitSemigroup: Semigroup[Unit] = semigroup[Unit]((_, _) => ())
+  implicit val UnitSemigroup: Semigroup[Unit] = semigroup((_, _) => ())
 
-  implicit val StringSemigroup: Semigroup[String] = semigroup[String](_ + _)
+  implicit val StringSemigroup: Semigroup[String] = semigroup(_ + _)
 
-  implicit val IntSemigroup: Semigroup[Int] = semigroup[Int](_ + _)
+  implicit val IntSemigroup: Semigroup[Int] = semigroup(_ + _)
 
-  implicit val IntMultiplicationSemigroup: Semigroup[IntMultiplication] = semigroup[IntMultiplication](_ * _ ∏)
+  implicit val IntMultiplicationSemigroup: Semigroup[IntMultiplication] = semigroup(_ * _ ∏)
 
-  implicit val BooleanConjunctionSemigroup: Semigroup[BooleanConjunction] = semigroup[BooleanConjunction](_ && _ |∧|)
+  implicit val BooleanConjunctionSemigroup: Semigroup[BooleanConjunction] = semigroup(_ && _ |∧|)
 
-  implicit val BooleanSemigroup: Semigroup[Boolean] = semigroup[Boolean]((a, b) => (a || b))
+  implicit val BooleanSemigroup: Semigroup[Boolean] = semigroup((a, b) => (a || b))
 
-  implicit val CharSemigroup: Semigroup[Char] = semigroup[Char]((a, b) => (a + b).toChar)
+  implicit val CharSemigroup: Semigroup[Char] = semigroup((a, b) => (a + b).toChar)
 
-  implicit val CharMultiplicationSemigroup: Semigroup[CharMultiplication] = semigroup[CharMultiplication]((a, b) => (a * b).toChar ∏)
+  implicit val CharMultiplicationSemigroup: Semigroup[CharMultiplication] = semigroup((a, b) => (a * b).toChar ∏)
 
-  implicit val ByteSemigroup: Semigroup[Byte] = semigroup[Byte]((a, b) => (a + b).toByte)
+  implicit val ByteSemigroup: Semigroup[Byte] = semigroup((a, b) => (a + b).toByte)
 
-  implicit val ByteMultiplicationSemigroup: Semigroup[ByteMultiplication] = semigroup[ByteMultiplication]((a, b) => (a * b).toByte ∏)
+  implicit val ByteMultiplicationSemigroup: Semigroup[ByteMultiplication] = semigroup((a, b) => (a * b).toByte ∏)
 
-  implicit val LongSemigroup: Semigroup[Long] = semigroup[Long]((a, b) => (a + b).toLong)
+  implicit val LongSemigroup: Semigroup[Long] = semigroup((a, b) => (a + b).toLong)
 
-  implicit val LongMultiplicationSemigroup: Semigroup[LongMultiplication] = semigroup[LongMultiplication]((a, b) => (a * b).toLong ∏)
+  implicit val LongMultiplicationSemigroup: Semigroup[LongMultiplication] = semigroup((a, b) => (a * b).toLong ∏)
 
-  implicit val ShortSemigroup: Semigroup[Short] = semigroup[Short]((a, b) => (a + b).toShort)
+  implicit val ShortSemigroup: Semigroup[Short] = semigroup((a, b) => (a + b).toShort)
 
-  implicit val ShortMultiplicationSemigroup: Semigroup[ShortMultiplication] = semigroup[ShortMultiplication]((a, b) => (a * b).toShort ∏)
+  implicit val ShortMultiplicationSemigroup: Semigroup[ShortMultiplication] = semigroup((a, b) => (a * b).toShort ∏)
 
-  implicit val FloatSemigroup: Semigroup[Float] = semigroup[Float]((a, b) => (a + b).toFloat)
+  implicit val FloatSemigroup: Semigroup[Float] = semigroup((a, b) => (a + b).toFloat)
 
-  implicit val DoubleSemigroup: Semigroup[Double] = semigroup[Double]((a, b) => (a + b).toDouble)
+  implicit val DoubleSemigroup: Semigroup[Double] = semigroup((a, b) => (a + b).toDouble)
 
-  implicit val BigIntegerSemigroup: Semigroup[java.math.BigInteger] = semigroup[java.math.BigInteger](_ add _)
+  implicit val BigIntegerSemigroup: Semigroup[java.math.BigInteger] = semigroup(_ add _)
 
-  implicit val BigIntegerMutliplicationSemigroup: Semigroup[BigIntegerMultiplication] = semigroup[BigIntegerMultiplication](_.value multiply _.value ∏)
+  implicit val BigIntegerMutliplicationSemigroup: Semigroup[BigIntegerMultiplication] = semigroup(_.value multiply _.value ∏)
 
-  implicit val BigIntSemigroup: Semigroup[BigInt] = semigroup[BigInt](_ + _)
+  implicit val BigIntSemigroup: Semigroup[BigInt] = semigroup(_ + _)
 
-  implicit val BigIntMultiplicationSemigroup: Semigroup[BigIntMultiplication] = semigroup[BigIntMultiplication](_.value * _.value ∏)
+  implicit val BigIntMultiplicationSemigroup: Semigroup[BigIntMultiplication] = semigroup(_.value * _.value ∏)
 
-  implicit val NodeSeqSemigroup: Semigroup[NodeSeq] = semigroup[NodeSeq](_ ++ _)
+  implicit val NodeSeqSemigroup: Semigroup[NodeSeq] = semigroup(_ ++ _)
 
-  implicit def NonEmptyListSemigroup[A]: Semigroup[NonEmptyList[A]] = semigroup[NonEmptyList[A]](_.list <::: _)
+  implicit def NonEmptyListSemigroup[A]: Semigroup[NonEmptyList[A]] = semigroup(_.list <::: _)
 
   // todo implicit def ZipStreamSemigroup[A] = semigroup[ZipStream[A]](_.value append _.value |!|)
 
-  implicit def ListSemigroup[A]: Semigroup[List[A]] = semigroup[List[A]](_ ::: _)
+  implicit def ListSemigroup[A]: Semigroup[List[A]] = semigroup(_ ::: _)
 
-  implicit def StreamSemigroup[A]: Semigroup[Stream[A]] = semigroup[Stream[A]](_ append _)
+  implicit def StreamSemigroup[A]: Semigroup[Stream[A]] = semigroup(_ append _)
 
-  implicit def OptionSemigroup[A]: Semigroup[Option[A]] = semigroup[Option[A]]((a, b) => if (a.isDefined) a else b)
+  implicit def OptionSemigroup[A]: Semigroup[Option[A]] = semigroup((a, b) => if (a.isDefined) a else b)
 
-  implicit def ArraySemigroup[A: Manifest]: Semigroup[Array[A]] = semigroup[Array[A]](Array.concat(_, _))
+  implicit def ArraySemigroup[A: Manifest]: Semigroup[Array[A]] = semigroup(Array.concat(_, _))
 
-  implicit def EitherLeftSemigroup[A, B]: Semigroup[Either.LeftProjection[A, B]] = semigroup[Either.LeftProjection[A, B]]((a, b) => if (a.e.isRight) a else b)
+  implicit def EitherLeftSemigroup[A, B]: Semigroup[Either.LeftProjection[A, B]] = semigroup((a, b) => if (a.e.isRight) a else b)
 
-  implicit def EitherRightSemigroup[A, B]: Semigroup[Either.RightProjection[B, A]] = semigroup[Either.RightProjection[B, A]]((a, b) => if (a.e.isLeft) a else b)
+  implicit def EitherRightSemigroup[A, B]: Semigroup[Either.RightProjection[B, A]] = semigroup((a, b) => if (a.e.isLeft) a else b)
 
-  implicit def Function1ABSemigroup[A, B](implicit s: Semigroup[B]): Semigroup[A => B] = semigroup[A => B]((a1, a2) => a => s append (a1(a), a2.apply(a)))
+  implicit def Function1ABSemigroup[A, B](implicit s: Semigroup[B]): Semigroup[A => B] = semigroup((a1, a2) => a => s append (a1(a), a2.apply(a)))
 
-  implicit def EndoSemigroup[A]: Semigroup[Endo[A]] = semigroup[Endo[A]]((x, y) => ((a: A) => x(y.apply(a))))
+  implicit def EndoSemigroup[A]: Semigroup[Endo[A]] = semigroup((x, y) => ((a: A) => x(y.apply(a))))
 
   implicit def DualSemigroup[A](implicit sa: Semigroup[A]): Semigroup[Dual[A]] =
-    semigroup[Dual[A]]((x, y) => dual(sa.append(y.value, x.value)))
+    semigroup((x, y) => dual(sa.append(y.value, x.value)))
 
   // todo  implicit def StrategySemigroup[A] = semigroup[Strategy[A]]((x, y) => ((a: () => A) => x(y.apply(a))))
 
   import java.util._
   import java.util.concurrent._
 
-  implicit def JavaArrayListSemigroup[A]: Semigroup[ArrayList[A]] = semigroup[ArrayList[A]]((a, b) => {
+  implicit def JavaArrayListSemigroup[A]: Semigroup[ArrayList[A]] = semigroup((a, b) => {
     val k = a.clone.asInstanceOf[ArrayList[A]]
     k addAll b
     k
   })
 
-  implicit def JavaLinkedListSemigroup[A]: Semigroup[LinkedList[A]] = semigroup[LinkedList[A]]((a, b) => {
+  implicit def JavaLinkedListSemigroup[A]: Semigroup[LinkedList[A]] = semigroup((a, b) => {
     val k = a.clone.asInstanceOf[LinkedList[A]]
     k addAll b
     k
   })
 
-  implicit def JavaPriorityQueueSemigroup[A]: Semigroup[PriorityQueue[A]] = semigroup[PriorityQueue[A]]((a, b) => {
+  implicit def JavaPriorityQueueSemigroup[A]: Semigroup[PriorityQueue[A]] = semigroup((a, b) => {
     val k = new PriorityQueue[A](a)
     k addAll b
     k
   })
 
-  implicit def JavaStackSemigroup[A]: Semigroup[Stack[A]] = semigroup[Stack[A]]((a, b) => {
+  implicit def JavaStackSemigroup[A]: Semigroup[Stack[A]] = semigroup((a, b) => {
     val k = a.clone.asInstanceOf[Stack[A]]
     k addAll b
     k
   })
 
-  implicit def JavaVectorSemigroup[A]: Semigroup[Vector[A]] = semigroup[Vector[A]]((a, b) => {
+  implicit def JavaVectorSemigroup[A]: Semigroup[Vector[A]] = semigroup((a, b) => {
     val k = a.clone.asInstanceOf[Vector[A]]
     k addAll b
     k
   })
 
-  implicit def JavaArrayBlockingQueueSemigroup[A]: Semigroup[ArrayBlockingQueue[A]] = semigroup[ArrayBlockingQueue[A]]((a, b) => {
+  implicit def JavaArrayBlockingQueueSemigroup[A]: Semigroup[ArrayBlockingQueue[A]] = semigroup((a, b) => {
     val k = new ArrayBlockingQueue[A](a.remainingCapacity + b.remainingCapacity)
     k addAll a
     k addAll b
     k
   })
 
-  implicit def JavaConcurrentLinkedQueueSemigroup[A]: Semigroup[ConcurrentLinkedQueue[A]] = semigroup[ConcurrentLinkedQueue[A]]((a, b) => {
+  implicit def JavaConcurrentLinkedQueueSemigroup[A]: Semigroup[ConcurrentLinkedQueue[A]] = semigroup((a, b) => {
     val k = new ConcurrentLinkedQueue[A](a)
     k addAll b
     k
   })
 
-  implicit def JavaCopyOnWriteArrayListSemigroup[A]: Semigroup[CopyOnWriteArrayList[A]] = semigroup[CopyOnWriteArrayList[A]]((a, b) => {
+  implicit def JavaCopyOnWriteArrayListSemigroup[A]: Semigroup[CopyOnWriteArrayList[A]] = semigroup((a, b) => {
     val k = a.clone.asInstanceOf[CopyOnWriteArrayList[A]]
     k addAll b
     k
   })
 
-  implicit def JavaLinkedBlockingQueueSemigroup[A]: Semigroup[LinkedBlockingQueue[A]] = semigroup[LinkedBlockingQueue[A]]((a, b) => {
+  implicit def JavaLinkedBlockingQueueSemigroup[A]: Semigroup[LinkedBlockingQueue[A]] = semigroup((a, b) => {
     val k = new LinkedBlockingQueue[A](a)
     k addAll b
     k
   })
 
-  implicit def JavaSynchronousQueueSemigroup[A]: Semigroup[SynchronousQueue[A]] = semigroup[SynchronousQueue[A]]((a, b) => {
+  implicit def JavaSynchronousQueueSemigroup[A]: Semigroup[SynchronousQueue[A]] = semigroup((a, b) => {
     val k = new SynchronousQueue[A]
     k addAll a
     k addAll b
