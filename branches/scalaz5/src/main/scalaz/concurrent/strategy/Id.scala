453ae427c2851e927c9e5ac3f8a938025b689657
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-29 23:36:43 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@1044 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/src/main/scalaz/concurrent/strategy/Id.scala
+++ branches/scalaz5/src/main/scalaz/concurrent/strategy/Id.scala
@@ -5,7 +5,7 @@ import scalaz.concurrent.Strategy
 /**
  * A strategy that performs no evaluation of its argument.
  */
-object Id {
+trait Id {
   implicit def strategy[A]: Strategy[A] = new Strategy[A] {
     def apply(a: () => A) = a
   }
--- branches/scalaz5/src/main/scalaz/concurrent/strategy/Naive.scala
+++ branches/scalaz5/src/main/scalaz/concurrent/strategy/Naive.scala
@@ -5,7 +5,7 @@ import scalaz.concurrent.Strategy
 /**
  * A simple strategy that spawns a new thread for every evaluation.
  */
-object Naive {
+trait Naive {
   import concurrent.ops._
   implicit def strategy[A]: Strategy[A] = new Strategy[A] {
     def apply(a: () => A) = future {a()}
