2652b59b3394e66d2e0951a8c13bb7385f57c26c
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-30 09:56:29 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@1066 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/src/example/scalaz/ExampleTraverse.scala
+++ branches/scalaz5/src/example/scalaz/ExampleTraverse.scala
@@ -4,6 +4,7 @@ object ExampleTraverse {
   def main(args: Array[String]) = run
 
   import Scalaz._
+  import collection.mutable.GenericArray
 
   def run {
     // Sequence the List with the Option applicative functor
@@ -34,13 +35,21 @@ object ExampleTraverse {
     some('1').traverseDigits println
 
     // Traverse a GenericArray of characters to get a possible GenericArray of digits (scalaz.Digit) using the Option applicative functor
-    collection.mutable.GenericArray('1', 'x', '3').traverseDigits println
+    GenericArray('1', 'x', '3').traverseDigits println
 
     // Traverse a List using the String monoid
     List(100, 200, 300) ↣ (_.toString) println
 
-    List(100, 200, 300) ↣ (x => x) println
+    // Traverse a GenericArray using the Int addition monoid
+    GenericArray(100, 200, 300) ↣ (x => x) println
   
-    List(100, 200, 300) ↣ (x => x ∏) println
+    // Traverse a Stream using the Int multiplication monoid
+    Stream(100, 200, 300) ↣ (x => x ∏) println
+
+    // Traverse an Option using the Int multiplication monoid
+    some(100) ↣ (x => x ∏) println
+
+    // Traverse an Option using the Int multiplication monoid
+    none[Long] ↣ (x => x ∏) println
   }
 }
