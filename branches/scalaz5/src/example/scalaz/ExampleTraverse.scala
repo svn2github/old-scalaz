a1d07cd4da8933ec17c1bc3142e4a99b96fd1ec0
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-11-30 06:54:56 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@1057 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/scalaz5/src/example/scalaz/ExampleTraverse.scala
+++ branches/scalaz5/src/example/scalaz/ExampleTraverse.scala
@@ -6,15 +6,25 @@ object ExampleTraverse {
   import Scalaz._
 
   def run {
+    // Sequence the List with the Option applicative functor
     println(List(Some(7), Some(9)).sequence[Option, Int]) // todo why must these type parameters be passed?
+
+    // Sequence the Stream with the Option applicative functor
     println(Stream(Some(7), None, Some(9)).sequence[Option, Int]) // todo why must these type parameters be passed?
 
     val f = (_: String).map(_ - 48).toList
     val g = (s: String) => (() => s.parseInt).throws.either.right.toOption
 
+    // Traverse the List with the Option functor (g)
     println(List("abc", "def") ↦ g)
+
+    // Traverse the List with the Option functor (g)
     println(List("7", "8") ↦ g)
+
+    // Traverse the Option with the Option functor (g)
     println(some("abc") ↦ g)
+
+    // Traverse the Option with the Option functor (g)
     println(some("9") ↦ g)
   }
 }
