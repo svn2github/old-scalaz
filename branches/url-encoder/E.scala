578b02febdb3560f61983aed09d7247df547c560
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2010-02-22 00:54:01 +0000

    git-svn-id: http://scalaz.googlecode.com/svn@1438 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- branches/url-encoder/E.scala
+++ branches/url-encoder/E.scala
@@ -4,5 +4,6 @@ object E {
       if(c.isLetter || c.isDigit || Set('!', '*', '\'', '(', ')', ';', ':', '@', '&', '=', '+', '$', ',', '/', '?', '%', '#', '[', ']').contains(c))
       List(c)
     else
-      c.toString getBytes "UTF-8" map (b => "%" + (b & 0xFF toHexString)) flatMap (z => z) toList) mkString
+      c.toString getBytes "UTF-8"
+        flatMap (b => "%" + (b & 0xFF toHexString)) toList) mkString
 }
