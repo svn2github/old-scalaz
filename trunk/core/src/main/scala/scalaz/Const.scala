a5cb8acfaba988763368f687afacab2598428ffb
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2010-04-10 02:45:47 +0000

    Delete trunk: see http://github.com/scalaz/scalaz
    git-svn-id: http://scalaz.googlecode.com/svn@1450 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- trunk/core/src/main/scala/scalaz/Const.scala
+++
@@ -1,8 +0,0 @@
-package scalaz
-
-/**
- * Used to generate Phantom Applicative Functors from a Monoidal type B.
- *
- * @see scalaz.Applicative#MonoidalApplicative
- */
-case class Const[B, +A](value: B) extends NewType[B] 
--- trunk/example/src/test/scala/scalaz/ExamplesTest.scala
+++
@@ -1,9 +0,0 @@
-package scalaz
-
-import org.specs.{Sugar, Specification}
-
-class ExamplesTest extends Specification with Sugar {
-  "run" in {
-    Example.run must_== () 
-  }
-}
